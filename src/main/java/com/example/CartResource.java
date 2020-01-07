7f28a4234084b59d24c316e0ca2a99d824387386
William Oliveira de Lagos <william.lagos@icloud.com>
2020-01-07 14:41:50 -0300

    Finished spring sample with rest operations

--- src/main/java/com/example/CartResource.java
+++ src/main/java/com/example/CartResource.java
@@ -1,7 +1,8 @@
 package com.example;
 
 import java.util.Optional;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,37 @@ public class CartResource {
    */
   public CartResource(CartRepository repository) {
     this.repository = repository;
+  }
+
+  @RequestMapping(value = "/compras/", 
+  method = RequestMethod.POST)
+  public Cart criarCompra(@RequestBody Cart cart) {
+    Product product = this.products.findById(
+      cart.getProduct().getId()).get();
+    Customer customer = this.customers.findById(
+      cart.getCustomer().getId()).get();
+    return this.repository.save(new Cart(product, customer));
+  }
+
+  @RequestMapping(value="/compras/{id}", 
+  method=RequestMethod.PUT)
+  public void alterarCompra(@PathVariable Long id,
+  @RequestBody Cart produtoParam) {
+      Cart compra = this.repository.findById(id).get();
+      compra.setProduct(this.products.findById(
+        produtoParam.getProduct().getId()).get());
+      compra.setCustomer(this.customers.findById(
+        produtoParam.getCustomer().getId()).get());
+      this.repository.save(compra);
+  }
+
+  @RequestMapping(value="/compras/finalizar",
+  method=RequestMethod.POST)
+  public Map<String,String> finalizarCompras() {
+    this.repository.deleteAll();
+    Map<String,String> res = new HashMap<>();
+    res.put("Mensagem", "Compra finalizada");
+    return res;
   }
 
   /**
@@ -56,24 +88,4 @@ public class CartResource {
   public void removerCompra(@PathVariable Long id) {
     this.repository.deleteById(id);
   }
-
-  @RequestMapping(value = "/compras/", 
-  method = RequestMethod.POST)
-  public Cart criarCompra(@RequestBody Cart cart) {
-    Product product = this.products.findById(
-      cart.getProduct().getId()).get();
-    Customer customer = this.customers.findById(
-      cart.getCustomer().getId()).get();
-    return this.repository.save(new Cart(product, customer));
-  }
-
-  @RequestMapping(value="/compras/{id}", 
-  method=RequestMethod.PUT)
-  public void alterarCompra(@PathVariable Long id,
-  @RequestBody Cart produtoParam) {
-      Cart cliente = this.repository.findById(id).get();
-      cliente.setProduct(produtoParam.getProduct());
-      cliente.setCustomer(produtoParam.getCustomer());
-  }
-
 }
